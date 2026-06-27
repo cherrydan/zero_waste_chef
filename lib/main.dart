b85bafce987cb99ccd841127e93394f209f8aaa5
cherrydan <cherrydan.1978@gmail.com>
2026-06-27 14:50:52 +0300

    chore: successfully integrate Firebase Core and initialize it in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
-import 'screens/fridge_screen.dart'; // Импортируем наш новый экран
+import 'package:firebase_core/firebase_core.dart'; // Импорт Firebase
+import 'firebase_options.dart'; // Настройки Firebase
+import 'screens/fridge_screen.dart'; // Наш главный экран
 
-void main() {
+void main() async {
+  // 1. Убеждаемся, что движок Flutter готов к работе
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 2. Инициализируем Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // 3. Запускаем приложение
   runApp(const MyApp());
 }
 
@@ -17,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const FridgeScreen(), // Запускаем приложение с экрана холодильника
+      home: const FridgeScreen(), // Запускаем с экрана холодильника
     );
   }
 }
